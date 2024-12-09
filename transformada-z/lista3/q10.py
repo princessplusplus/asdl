fbdba46f3c262538b21208a137caea6b5da4b69b
princessplusplus <stephanie14@outlook.com>
2024-12-09 22:56:44 +0000

    questão 10

--- transformada-z/lista3/q10.py
+++ transformada-z/lista3/q10.py
@@ -11,6 +11,7 @@ X_z = (2*z)/((z-1)*(z+1))
 X_frac = X_z.partfrac()
 sp.pprint(X_frac)
 x_n = X_frac(n, causal=True)
+print("x[n]:")
 sp.pprint(x_n)
 
 print("\n") 
@@ -21,6 +22,7 @@ X_z = (2*z)/((z-1)*(z+1))
 X_frac = X_z.partfrac()
 sp.pprint(X_frac)
 x_n = X_frac(n, causal=False)
+print("x[n]:")
 sp.pprint(x_n)
 
 print("\n") 
@@ -31,6 +33,7 @@ X_z = (-2.5*z)/((z-0.5)*(z+2))
 X_frac = X_z.partfrac()
 sp.pprint(X_frac)
 x_n = X_frac(n, causal=True)
+print("x[n]:")
 sp.pprint(x_n)
 
 print("\n") 
@@ -41,6 +44,7 @@ X_z = (-2.5*z)/((z-0.5)*(z+2))
 X_frac = X_z.partfrac()
 sp.pprint(X_frac)
 x_n = X_frac(n, causal=False)
+print("x[n]:")
 sp.pprint(x_n)
 
 print("\n") 
@@ -58,6 +62,7 @@ X2_z = (-1/2)/(z-(1/2))
 x1_n = X1_z(n, causal=False)
 x2_n = X2_z(n)
 x_n = x1_n + x2_n
+print("x[n]:")
 sp.pprint(x_n)
 
 print("\n") 
@@ -66,6 +71,7 @@ print("f)")
 # X_z = (z+1)/(z+(1/3)), |z| > 1/3
 X_z = (z+1)/(z+(sp.Rational(1, 3)))
 x_n = X_z(n)
+print("x[n]:")
 sp.pprint(x_n.simplify())
 
 print("\n") 
@@ -99,11 +105,55 @@ sp.pprint(x_n.simplify())
 print("\n") 
 print("------------------------------------------------")
 print("h)")
+# X_z = (z**3 + z**2 + (3/2)*z - (1/2))/((z**3 + (3/2)*(z**2) + (1/2)*z)), |z| < 0.5
+# Fazer divisão longa de polinômios
+X1_z = 1
+X2_z = (-1/2) * z**(-1)
+X3_z = ((7/4)*z + (3/4))/(z**3 + (3/2)*(z**2) + (1/2)*z) 
+X_fat = X3_z.factored(pairs=True)
+print("Fatorado:")
+sp.pprint(X_fat)
+print("\n") 
+print("Frações parciais:")
+X_frac = X_fat.partfrac()
+sp.pprint(X_frac)
+fractions = X_frac.args  
+fraction1, fraction2, fraction3 = fractions
+#sp.pprint(fraction1)
+#sp.pprint(fraction2)
+#sp.pprint(fraction3)
+X31_z = -2/(z+1)
+X32_z = (1/2)/(z+(1/2))
+X33_z = (3/2)/(z)
+x3_n = X31_z + X32_z + X33_z
+x_n = delta(n) + X2_z(n) + X31_z(n, causal=False) + X32_z(n) + X33_z(n, causal=False)
+sp.pprint(x_n.simplify())
+
 
 print("\n") 
 print("------------------------------------------------")
 print("i)")
-
+# X_z = (z**3 + z**2 + (3/2)*z - (1/2))/((z**2 + (3/2)*z + (1/2))), |z| > 1
+# Fazer divisão longa de polinômios
+X1_z = 1
+X2_z = (-1/2)
+X3_z = ((7/4)*z + (3/4))/(z**2 + (3/2)*z + (1/2)) 
+X_fat = X3_z.factored(pairs=True)
+print("Fatorado:")
+sp.pprint(X_fat)
+print("\n") 
+print("Frações parciais:")
+X_frac = X_fat.partfrac()
+sp.pprint(X_frac)
+fractions = X_frac.args  
+fraction1, fraction2 = fractions
+#sp.pprint(fraction1)
+#sp.pprint(fraction2)
+X31_z = 2/(z+1)
+X32_z = (-1/4)/(z+(1/2))
+x3_n = X31_z + X32_z
+x_n = delta(n) + X2_z * delta(n) + X31_z(n) + X32_z(n)
+sp.pprint(x_n.simplify())
 
 time.sleep(20)
 os.system('clear')
