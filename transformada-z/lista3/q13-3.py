d5982556d2ea2f75832cd2bf1ba514b4edf24286
princessplusplus <stephanie14@outlook.com>
2024-12-09 17:01:26 +0000

    atualizacao 00

--- transformada-z/lista3/q13-3.py
+++ transformada-z/lista3/q13-3.py
@@ -4,5 +4,5 @@ import sympy as sp
 x2_n = -27 * (1/2)**(n-1) * u(n-1) + (9/(1/2)) * (n-1) * (1/2)**(n-1) * u(n-1) + 27 * (-1/2)**(n-1) * u(n-1) + (18/(-1/2)) * (n-1) * (-1/2)**(n-1) * u(n-1) -  36 * (n-1) * (n-2) * (-1/2)**(n) * u(n-1)
 
 # Calcular o somatório de x2_n de n = -infinito até n = +infinito
-resultado = sp.summation(x2_n, (n, -sp.infinito, sp.infinito))
+resultado = sp.summation(x2_n, (n, -sp.oo, sp.oo))
 print(resultado)
