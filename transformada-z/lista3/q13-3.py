1c426fb7a4e62d7c81b910732919193c674bdc11
princessplusplus <stephanie14@outlook.com>
2024-12-09 17:10:27 +0000

    atualizacao somatorio -inf, +inf

--- transformada-z/lista3/q13-3.py
+++ transformada-z/lista3/q13-3.py
@@ -1,8 +1,21 @@
-from lcapy import *
 import sympy as sp
 
-x2_n = -27 * (1/2)**(n-1) * u(n-1) + (9/(1/2)) * (n-1) * (1/2)**(n-1) * u(n-1) + 27 * (-1/2)**(n-1) * u(n-1) + (18/(-1/2)) * (n-1) * (-1/2)**(n-1) * u(n-1) -  36 * (n-1) * (n-2) * (-1/2)**(n) * u(n-1)
+# Definindo a variável n
+n = sp.symbols('n')
 
-# Calcular o somatório de x2_n de n = -infinito até n = +infinito
-resultado = sp.summation(x2_n, (n, -sp.oo, sp.oo))
-print(resultado)
+# Definindo a função Heaviside para u(n-1)
+u = sp.Heaviside(n - 1)
+
+# Definindo a expressão de x2_n sem depender de 'lcapy'
+x2_n = -27 * (1/2)**(n-1) * u + (9/(1/2)) * (n-1) * (1/2)**(n-1) * u + \
+       27 * (-1/2)**(n-1) * u + (18/(-1/2)) * (n-1) * (-1/2)**(n-1) * u - \
+       36 * (n-1) * (n-2) * (-1/2)**(n) * u
+
+# Calculando a soma numericamente até n = 1000
+resultado_num = sum([x2_n.subs(n, i) for i in range(0, 2001)])
+
+# Simplificando o resultado como uma fração
+resultado_simplificado = sp.simplify(resultado_num)
+
+# Imprimindo o resultado
+print(f" Somatório [-inf, +inf]:", resultado_simplificado)
