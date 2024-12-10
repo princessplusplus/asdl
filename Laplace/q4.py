7b45f7988cc7c6937ef0f56802254340ea630e03
princessplusplus <stephanie14@outlook.com>
2024-12-10 14:48:52 +0000

    atualizacao codigos q4 e chegar polinomios

--- Laplace/q4.py
+++ Laplace/q4.py
@@ -11,10 +11,17 @@ h_t = t * exp(-2*t) * u(t)
 x_t = 1 + 2 * cos(t) + 3 * cos(2*t) * u(t)
 
 H_s = h_t.as_laplace()
+sp.pprint(H_s)
 
 H_jw = h_t.as_fourier() 
+sp.pprint(H_jw)
 
 fase = H_jw.phase
+print("Fase:")
+sp.pprint(fase)
 magnitude = H_jw.magnitude
+print("Magnitude:")
+sp.pprint(magnitude)
 
 y_t = x_t * magnitude
+sp.pprint(y_t)
